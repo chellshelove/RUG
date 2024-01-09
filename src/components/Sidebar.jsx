06b3b10392bcda7901e7639378529fae501b3166
Avariq Fazlur Rahman <avariq.rahman001@binus.ac.id>
2024-01-09 23:55:00 +0700

    added recognition text on bottom

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,22 +1,34 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "./Sidebar.css";
 import Logo from "../imgs/logo.png";
 import { UilSignOutAlt } from "@iconscout/react-unicons";
 import { SidebarData } from "../Data/Data";
 import { UilBars } from "@iconscout/react-unicons";
 import { motion } from "framer-motion";
-import { signOut } from "firebase/auth";
+import { signOut, onAuthStateChanged } from "firebase/auth";
 import { auth } from "../Firebase";
 import { SidebarButtonContext } from "../App";
 
 const Sidebar = () => {
   const [selected, setSelected] = useContext(SidebarButtonContext);
   const [expanded, setExpanded] = useState(true);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   const logout = () => {
     signOut(auth);
   };
-
 
   const sidebarVariants = {
     true: {
@@ -26,43 +38,47 @@ const Sidebar = () => {
       left : '-60%'
     }
   }
-  // console.log(window.innerWidth)
+
   return (
     <>
       <div className="bars" style={expanded?{left: '60%'}:{left: '5%'}} onClick={()=>setExpanded(!expanded)}>
         <UilBars />
       </div>
-    <motion.div className='sidebar'
-    variants={sidebarVariants}
-    animate={window.innerWidth<=768?`${expanded}`:''}
-    >
-      {/* logo */}
-      <div className="logo">
-        <img src={Logo} alt="logo" />
-        <span>
-          R<span>U</span>G
-        </span>
-      </div>
-
-      <div className="menu">
-        {SidebarData.map((item, index) => {
-          return (
-            <div
-              className={selected === item.heading ? "menuItem active" : "menuItem"}
-              // key={index}
-              onClick={() => setSelected(item.heading)}
-            >
-              <item.icon />
-              <span>{item.heading}</span>
-            </div>
-          );
-        })}
-        {/* signoutIcon */}
-        <div className="menuItem">
-          <UilSignOutAlt onClick={logout}/>
+      <motion.div className='sidebar'
+        variants={sidebarVariants}
+        animate={window.innerWidth<=768?`${expanded}`:''}
+      >
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+          <span>
+            R<span>U</span>G
+          </span>
         </div>
-      </div>
-    </motion.div>
+
+        <div className="menu">
+          {SidebarData.map((item, index) => {
+            return (
+              <div
+                className={selected === item.heading ? "menuItem active" : "menuItem"}
+                onClick={() => setSelected(item.heading)}
+              >
+                <item.icon />
+                <span>{item.heading}</span>
+              </div>
+            );
+          })}
+
+          {user && (
+            <div className="menuItem user">
+              <span>Who's logged in? {user.email}</span>
+            </div>
+          )}
+
+          <div className="menuItem">
+            <UilSignOutAlt onClick={logout}/>
+          </div>
+        </div>
+      </motion.div>
     </>
   );
 };
