aeae6fcda68aae625a75043198b27c74a4fe01df
Avariq Fazlur Rahman <avariq.rahman001@binus.ac.id>
2024-01-09 23:44:10 +0700

    data change lol

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -95,19 +95,19 @@ export const UpdatesData = [
   {
     img: img1,
     name: "Andrew Thomas",
-    noti: "has ordered Apple smart watch 2500mh battery.",
+    noti: "has ordered Apple.",
     time: "25 seconds ago",
   },
   {
     img: img2,
     name: "James Bond",
-    noti: "has received Samsung gadget for charging battery.",
+    noti: "has received Orange.",
     time: "30 minutes ago",
   },
   {
     img: img3,
-    name: "Iron Man",
-    noti: "has ordered Apple smart watch, samsung Gear 2500mh battery.",
+    name: "Wendy",
+    noti: "has ordered Pear.",
     time: "2 hours ago",
   },
 ];
